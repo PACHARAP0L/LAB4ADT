b65634892f454c10387b37ce28a53717243e160a
PACHARAP0L <pacharapol.sr@ku.th>
2025-07-18 18:20:12 +0700

    Add files via upload

---
+++ IntegerSetTest.java
@@ -0,0 +1,46 @@
+
+public class IntegerSetTest {
+    public static void main(String[] args) {
+        int passed = 0;
+        int failed = 0;
+
+        IntegerSet a = new IntegerSet();
+        a.add(50);
+        a.add(70);
+        String s = "[50, 70]";
+        if (a.toString().equals(s)) {
+            System.out.println("Pass");
+            passed++;
+        }
+        else{
+            System.out.println("Fail "+ a.toString());failed++;
+        }
+        
+
+        IntegerSet b = new IntegerSet();
+        b.add(50);
+        b.add(80);
+        String s1 = "[50, 80]";
+        
+        if (b.toString().equals(s1)) {
+            System.out.println("Pass");
+            passed++;
+        }
+        else{
+            System.out.println("Fail "+ b.toString()); failed++;
+        }
+        
+
+        
+        // --- สรุปผล ---
+        System.out.println("\n--------------------");
+        System.out.println("--- Test Summary ---");
+        System.out.println("Passed: " + passed + ", Failed: " + failed);
+        if (failed == 0) {
+            System.out.println("Excellent! All tests passed!");
+        } else {
+            System.out.println("Some tests failed.");
+        }
+
+    }
+}
